03d71e548219cc6c8b5886e7ad276093cb391015
JaeHyung <cupr789@naver.com>
2018-03-13 09:46:19 +0900

    2018-03-13 gitIgnore함

---
+++ hungrybunny/src/main/java/co/kr/hungrybunny/controller/MapController.java
@@ -0,0 +1,29 @@
+package co.kr.hungrybunny.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import co.kr.hungrybunny.vo.MenuVO;
+
+@Controller
+@RequestMapping("/map")
+public class MapController {
+	@RequestMapping(value="/mapList", method=RequestMethod.POST)
+	public @ResponseBody ModelAndView getMenuList(@RequestBody Map<String,Object> map ,ModelAndView mav){
+		
+		
+		System.out.println("검색한 값: "+map.get("address"));
+		
+		mav.addObject("address",map.get("address"));
+		mav.setViewName("map/firstMap");
+		return mav;
+	}
+}
