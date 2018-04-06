e836f4b93f37e10afc4443913b881713efa30c9d
DJA <DJA@DJA-PC>
2018-04-06 10:05:49 +0900

    adsf

--- hungrybunny/src/main/java/co/kr/hungrybunny/ws/GetHttpSessionConfigurator.java
+++ hungrybunny/src/main/java/co/kr/hungrybunny/ws/GetHttpSessionConfigurator.java
@@ -5,12 +5,17 @@ import javax.websocket.HandshakeResponse;
 import javax.websocket.server.HandshakeRequest;
 import javax.websocket.server.ServerEndpointConfig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GetHttpSessionConfigurator extends ServerEndpointConfig.Configurator{
+	private static final Logger log = LoggerFactory.getLogger(GetHttpSessionConfigurator.class);
     @Override
     public void modifyHandshake(ServerEndpointConfig config, 
                                 HandshakeRequest request, 
                                 HandshakeResponse response){
         HttpSession httpSession = (HttpSession)request.getHttpSession();
+        log.info("modifyHandshake() -> httpsession => {}", httpSession); 
         System.out.println(HttpSession.class.getName());
         config.getUserProperties().put(HttpSession.class.getName(),httpSession);
     }
